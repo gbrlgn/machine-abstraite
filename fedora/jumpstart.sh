3f9f96b47cc8189e65b5cea469d3999effa72e8d
Gabriel Gian <gabrielgian@protonmail.com>
2021-11-06 13:36:19 -0300

    Dancer/Danger

--- fedora/jumpstart.sh
+++ fedora/jumpstart.sh
@@ -11,7 +11,7 @@ echo "excludepkgs=gnome-tour" >> /etc/dnf/dnf.conf
 ###########################################################
 
 
-dnf install -y acpid akmod-nvidia clisp dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core elixir fontawesome-fonts gcc geary ghc glib2-devel gnome-extensions-app gnome-music gnome-tweaks go hanazono-fonts htop ibm-plex-fonts-all java-latest-openjdk java-latest-openjdk-devel keepassxc kernel-devel kernel-headers libglvnd-devel libglvnd-glx libglvnd-opengl linux-firmware make npm pkgconfig php pip powerline powerline-fonts rust shotwell util-linux-user neovim vulkan-headers vulkan-tools xorg-x11-drv-nvidia-cuda zsh
+dnf install -y acpid akmod-nvidia clisp cpp dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core elixir epiphany fontawesome-fonts gcc geary ghc glib2-devel gnome-extensions-app gnome-music gnome-tweaks go hanazono-fonts htop ibm-plex-fonts-all java-latest-openjdk java-latest-openjdk-devel keepassxc kernel-devel kernel-devel-matched kernel-headers libglvnd-devel libglvnd-glx libglvnd-opengl linux-firmware make neovim NetworkManager-tui newsflash npm pkgconfig php pip powerline powerline-fonts rust shotwell util-linux-user vulkan-headers vulkan-tools wike xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-libs.x86_64 zsh
 
 
 dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
@@ -20,8 +20,10 @@ dnf install -y lame\* --exclude=lame-devel
 
 dnf group upgrade --with-optional Multimedia
 
-dnf remove -y cheese cockpit eog gnome-abrt gnome-calendar gnome-clocks gnome-connections gnome-contacts gnome-maps gnome-photos gnome-system-monitor gnome-tour gnome-user-docs gnome-weather	mediawriter rhythmbox yelp    
+dnf remove -y cheese cockpit eog gnome-abrt gnome-clocks gnome-connections gnome-contacts gnome-photos gnome-system-monitor gnome-tour gnome-user-docs gnome-weather mediawriter rhythmbox yelp
 
+
+flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark 
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.vscodium.codium
 flatpak install flathub org.gnome.Fractal
@@ -52,18 +54,6 @@ flatpak override --env=FLATPAK_ENABLE_SDK_EXT="*" com.vscodium.codium
 ###########################################################
 
 
-curl -o /home/danger/Documentos/NVIDIA-Linux-x86_64-495.44.run -L https://us.download.nvidia.com/XFree86/Linux-x86_64/495.44/NVIDIA-Linux-x86_64-495.44.run
-
-systemctl set-default multi-user.target
-
-bash /home/danger/Documentos/NVIDIA-Linux-*.run
-
-systemctl set-default graphical.target
-
-
-###########################################################
-
-
 echo "dancer" > /etc/hostname
 echo "blacklist nouveau \
 	options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
@@ -83,7 +73,7 @@ cd /home/danger/Documentos/Git/compsci && git config credential.helper store
 git clone https://github.com/gbrlgian/books /home/danger/Documentos/Git/books
 cd /home/danger/Documentos/Git/books && git config credential.helper store
 
-git clone https://github.com/gbrlgian/compsci /home/danger/Documentos/Git/machine-abstraite
+git clone https://github.com/gbrlgian/machine-abstraite /home/danger/Documentos/Git/machine-abstraite
 cd /home/danger/Documentos/Git/machine-abstraite && git config credential.helper store
 
 git clone https://github.com/gbrlgian/private /home/danger/Documentos/Git/private
@@ -128,8 +118,14 @@ sh -c /home/danger/Documentos/Git/machine-abstraite/inconfidente/terminal-color.
 
 ###########################################################
 
+
+rm /usr/share/applications/java-17-openjdk*
+rm /usr/share/applications/org.gnome.Tour.desktop
+
+
+###########################################################
+
+
 passwd -l root
 dnf clean all
 chown -R danger /home/danger
-
-reboot
