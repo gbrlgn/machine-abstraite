97340f3df41cd47e24ed969541e442a5c4bc6bbd
Gabriel Gian <gabrielgian@protonmail.com>
2021-11-30 19:40:12 -0300

    Aux chiottes

--- fedora/jumpstart.sh
+++ fedora/jumpstart.sh
@@ -2,16 +2,16 @@ dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "fastestmirror=True" >> /etc/dnf/dnf.conf
-echo "deltarpm=True" >> /etc/dnf/dnf.conf
-echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-echo "excludepkgs=gnome-tour" >> /etc/dnf/dnf.conf
+echo "fastestmirror=True \
+	deltarpm=True \
+	max_parallel_downloads=10 \
+	excludepkgs=gnome-tour" >> /etc/dnf/dnf.conf
 
 
 ###########################################################
 
 
-dnf install -y acpid akmod-nvidia binutils clisp coreutils cpp dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core elixir epiphany ffmpeg ffmpeg-devel ffmpeg-libs fontawesome-fonts gcc geary ghc glib2-devel gnome-extensions-app gnome-music gnome-tweaks go hanazono-fonts htop ibm-plex-fonts-all java-latest-openjdk java-latest-openjdk-devel keepassxc kernel-devel kernel-devel-matched kernel-headers libglvnd-devel libglvnd-glx libglvnd-opengl libvirt linux-firmware make neovim NetworkManager-tui newsflash npm pkgconfig php pip powerline powerline-fonts R rstudio rstudio-gui rust shotwell steam util-linux-user vulkan-headers vulkan-tools wike xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-libs.x86_64 zsh
+dnf install -y acpid akmod-nvidia binutils clisp coreutils cpp dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core elixir epiphany ffmpeg ffmpeg-devel ffmpeg-libs fontawesome-fonts gcc geary ghc glib2-devel gnome-extensions-app gnome-music gnome-tweaks go hanazono-fonts htop ibm-plex-fonts-all java-latest-openjdk java-latest-openjdk-devel kernel-devel kernel-devel-matched kernel-headers libglvnd-devel libglvnd-glx libglvnd-opengl libvirt linux-firmware make neovim NetworkManager-tui newsflash npm pkgconfig php pip powerline powerline-fonts R rstudio rstudio-gui rust shotwell steam util-linux-user vulkan-headers vulkan-tools wike xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-libs.x86_64 zsh
 
 
 dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
@@ -25,32 +25,40 @@ dnf remove -y cheese cockpit eog gnome-abrt gnome-clocks gnome-connections gnome
 dnf install -y https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 
-flatpak install flathub io.github.celluloid_player.Celluloid
 flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark 
+flatpak install flathub io.github.celluloid_player.Celluloid
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.vscodium.codium
+flatpak install flathub com.github.gi_lom.dialect
+flatpak install flathub org.gustavoperedo.FontDownloader
 flatpak install flathub org.gnome.Fractal
 flatpak install flathub de.haeckerfelix.Fragments
 flatpak install flathub org.freedesktop.Sdk.Extension.gcc8
 flatpak install flathub org.gimp.GIMP
 flatpak install flathub org.freedesktop.Sdk.Extension.golang
+flatpak install flathub dev.geopjr.Hashbrown
 flatpak install flathub org.freedesktop.Sdk.Extension.haskell
 flatpak install flathub org.inkscape.Inkscape
 flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+flatpak install flathub io.github.seadve.Kooha
 flatpak install flathub org.freedesktop.Sdk.Extension.llvm12
+flatpak install flathub fr.romainvigier.MetadataCleaner
 flatpak install flathub org.freedesktop.Sdk.Extension.node16
 flatpak install flathub org.freedesktop.Sdk.Extension.openjdk17
-flatpak install flathub com.jetbrains.PyCharm-Community
+flatpak install flathub org.gnome.PasswordSafe
 flatpak install flathub org.freedesktop.Sdk.Extension.php80
+flatpak install flathub org.gnome.Podcasts
+flatpak install flathub com.jetbrains.PyCharm-Community
 flatpak install flathub org.freedesktop.Sdk.Extension.rust-stable
+flatpak install flathub re.sonny.Tangram
+flatpak install flathub com.github.liferooter.textpieces
 flatpak install flathub org.freedesktop.Sdk.Extension.toolchain-aarch64
 
 
 flatpak override --env=FLATPAK_ENABLE_SDK_EXT="*" com.jetbrains.IntelliJ-IDEA-Community
-
 flatpak override --env=FLATPAK_ENABLE_SDK_EXT="*" com.jetbrains.PyCharm-Community
-
 flatpak override --env=FLATPAK_ENABLE_SDK_EXT="*" com.vscodium.codium
+
 
 flatpak upgrade
 
@@ -58,7 +66,7 @@ flatpak upgrade
 ###########################################################
 
 
-echo "dancer" > /etc/hostname
+echo "danger" > /etc/hostname
 echo "blacklist nouveau \
 	options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
 dracut -v -f
@@ -67,39 +75,40 @@ dracut -v -f
 ###########################################################
 
 
-git config --global user.name "Gabriel Gian"
-git config --global user.email "gabrielgian@protonmail.com"
+sudo -u dancer git config --global user.name "Gabriel Gian"
+sudo -u dancer git config --global user.email "gabrielgian@protonmail.com"
 
 
-git clone https://github.com/gbrlgian/compsci /home/danger/Documentos/Git/compsci
-cd /home/danger/Documentos/Git/compsci && git config credential.helper store
+git clone https://github.com/gbrlgian/compsci /home/dancer/Documentos/Git/compsci
+cd /home/dancer/Documentos/Git/compsci && git config credential.helper store
 
-git clone https://github.com/gbrlgian/books /home/danger/Documentos/Git/books
-cd /home/danger/Documentos/Git/books && git config credential.helper store
+git clone https://github.com/gbrlgian/books /home/dancer/Documentos/Git/books
+cd /home/dancer/Documentos/Git/books && git config credential.helper store
 
-git clone https://github.com/gbrlgian/machine-abstraite /home/danger/Documentos/Git/machine-abstraite
-cd /home/danger/Documentos/Git/machine-abstraite && git config credential.helper store
+git clone https://github.com/gbrlgian/machine-abstraite /home/dancer/Documentos/Git/machine-abstraite
+cd /home/dancer/Documentos/Git/machine-abstraite && git config credential.helper store
 
-git clone https://github.com/gbrlgian/private /home/danger/Documentos/Git/private
-cd /home/danger/Documentos/Git/private && git config credential.helper store
+git clone https://github.com/gbrlgian/private /home/dancer/Documentos/Git/private
+cd /home/dancer/Documentos/Git/private && git config credential.helper store
+
+
+###########################################################
+
+
+chsh -s /bin/zsh dancer
+
+sudo -u dancer sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir -p /home/dancer/.zsh
+git clone https://github.com/sindresorhus/pure.git /home/dancer/.zsh/pure
+
+cp /home/dancer/Documentos/Git/machine-abstraite/config/.zshrc /home/dancer
 
 
 ###########################################################
 
 
-chsh -s /bin/zsh danger
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-mkdir -p /home/danger/.zsh
-git clone https://github.com/sindresorhus/pure.git /home/danger/.zsh/pure
-
-cp /home/danger/Documentos/Git/machine-abstraite/config/.zshrc /home/danger/
-
-
-###########################################################
-
-cd /home/danger/.config
+cd /home/dancer/.config
 
 ausearch -c 'hl2_linux' --raw | audit2allow -M hl2linux
 
@@ -109,23 +118,24 @@ semodule -X 300 -i hl2linux.pp
 ###########################################################
 
 
-cp -r /home/danger/Documentos/Git/machine-abstraite/Wallpapers /home/danger/Imagens
+cp -r /home/dancer/Documentos/Git/machine-abstraite/Wallpapers /home/dancer/Imagens
 
-cp /home/danger/Documentos/Git/machine-abstraite/config/desktop/nvidia-powermizer.desktop /home/danger/.config/autostart/
+cp /home/dancer/Documentos/Git/machine-abstraite/config/nvidia-powermizer.desktop /home/dancer/.config/autostart
 
 
 ###########################################################
 
 
 rm /usr/share/applications/java*
+rm /usr/share/applications/openjdk*
 rm /usr/share/applications/org.gnome.Tour.desktop
 
 
 ###########################################################
 
 dnf upgrade -y
+dnf clean all
 flatpak upgrade
 
 passwd -l root
-dnf clean all
-chown -R danger /home/danger
+chown -R dancer /home/dancer
