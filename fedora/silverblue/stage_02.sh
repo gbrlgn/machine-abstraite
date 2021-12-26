d18bc22cca72be75fce5948d22bbb97a4140506f
Gabriel Gian <gabrielgian@protonmail.com>
2021-12-26 09:01:29 -0300

    Trois Couleurs: Blanc

--- fedora/silverblue/stage_02.sh
+++ fedora/silverblue/stage_02.sh
@@ -1,3 +1,3 @@
-rpm-ostree upgrade --install acpid akmod-nvidia dbus-devel dbus-glib-devel gnome-tweaks dkms emacs glib2-devel gnome-boxes htop kernel-devel-matched lame libglvnd-devel NetworkManager-tui steam vulkan-headers vulkan-tools xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs xorg-x11-drv-nvidia-libs.x86_64 zsh
+rpm-ostree upgrade --install acpid akmod-nvidia dbus-devel dbus-glib-devel ffmpeg gnome-tweaks dkms emacs glib2-devel gnome-boxes gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav htop kernel-devel-matched lame libglvnd-devel NetworkManager-tui steam vulkan-headers vulkan-tools xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs xorg-x11-drv-nvidia-libs.x86_64 zsh --allow-inactive
 
 rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
