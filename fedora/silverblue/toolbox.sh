c7acccb4f08ba1e963022b9f0139b051d042105e
Gabriel Gian <gabrielgian@protonmail.com>
2021-12-29 15:55:02 -0300

    Des jours meilleurs viendront

--- fedora/silverblue/toolbox.sh
+++ fedora/silverblue/toolbox.sh
@@ -1,6 +1,12 @@
+# Default
+
 echo "fastestmirror=True \
 deltarpm=True \
 max_parallel_downloads=10 \
 excludepkgs=gnome-tour" >> /etc/dnf/dnf.conf
 	
 dnf install -y binutils clisp coreutils cpp dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core elixir ffmpeg ffmpeg-devel ffmpeg-libs fontawesome-fonts gcc ghc glib2-devel go hanazono-fonts htop ibm-plex-fonts-all java-latest-openjdk java-latest-openjdk-devel kernel-devel kernel-devel-matched kernel-headers linux-firmware make neovim NetworkManager-tui npm pkgconfig php pip powerline powerline-fonts rust util-linux-user zsh
+
+# Clojure
+	
+sudo dnf install -y binutils clojure dkms dnf-plugins-core java-latest-openjdk java-latest-openjdk-devel kernel-devel kernel-devel-matched kernel-headers leiningen linux-firmware make
