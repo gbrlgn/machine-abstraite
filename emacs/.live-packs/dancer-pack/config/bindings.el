6eb37d140859e12c96d8164e873d01c63ed54b5c
Gabriel Gian <gabrielgian@protonmail.com>
2022-04-02 08:46:34 -0300

    make install

--- emacs/.live-packs/dancer-pack/config/bindings.el
+++
@@ -1,8 +0,0 @@
-;; Place your bindings here.
-
-(global-set-key (kbd "<C-up>") 'shrink-window)
-(global-set-key (kbd "<C-down>") 'enlarge-window)
-(global-set-key (kbd "<C-left>") 'shrink-window-horizontal)
-(global-set-key (kbd "<C-right>") 'enlarge-window-horizontal)
-
-(define-key global-map (kbd "RET") 'newline-and-indent)
