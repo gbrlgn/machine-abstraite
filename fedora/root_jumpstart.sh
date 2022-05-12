6d92e532e17dcd067537e91ebec5708559dabf7c
Gabriel Gian <gabrielgian@protonmail.com>
2022-05-11 23:46:55 -0300

    Desolé

--- fedora/root_jumpstart.sh
+++ fedora/root_jumpstart.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 dnf upgrade -y
 
 
@@ -6,9 +8,9 @@ dnf upgrade -y
 
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf install -y acpid binutils coreutils dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core emacs exa fish fontawesome-fonts glib2-devel gnome-tweaks hanazono-fonts htop libtool kernel-devel kernel-devel-matched kernel-headers libglvnd-devel libglvnd-glx libglvnd-opengl linux-firmware make NetworkManager-tui p7zip pciutils powerline-fonts toolbox unrar util-linux-user vulkan-headers vulkan-tools
+dnf install -y acpid binutils coreutils dbus-devel dbus-glib-devel dbus-libs dkms dnf-plugins-core emacs exa fish fontawesome-fonts glib2-devel gnome-tweaks hanazono-fonts htop libtool kernel-devel kernel-devel-matched kernel-headers linux-firmware make NetworkManager-tui p7zip pciutils powerline-fonts snapd toolbox util-linux-user vulkan-headers vulkan-tools
 
-dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 --exclude=gstreamer1-plugins-bad-free-devel
 
 dnf install -y lame\* --exclude=lame-devel
 
@@ -20,12 +22,13 @@ dnf remove -y cheese cockpit eog gnome-abrt gnome-clocks gnome-connections gnome
 ###########################################################
 
 
-echo "pleroma" > /etc/hostname
+echo "syzygy" > /etc/hostname
 usermod -s /bin/fish gbrlgn
 rm /usr/share/applications/org.gnome.Tour.desktop
 
 
 ###########################################################
+
 
 flatpak upgrade --assumeyes
 dnf autoremove
---
+++ fedora/snap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo ln -s /var/lib/snapd/snap /snap
+snap install intellij-idea-community --classic
+snap install pycharm-community --classic
+snap install codium --classic
+snap install onlyoffice-desktopeditors
