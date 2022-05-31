35ff6df12bced6b9f69a6603ade6b8c2ba178bf3
Gabriel Gian <gabrielgian@protonmail.com>
2022-05-31 17:00:00 -0300

    It does not suffice

--- ubuntu/root_jumpstart.sh
+++ ubuntu/root_jumpstart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update && apt upgrade
 
-apt install -y acpid awscli binutils cargo celluloid clojure curl dkms docker-compose elixir ffmpeg fish fonts-firacode fonts-ibm-plex gcc golang htop linux-firmware make mesa-vulkan-drivers make npm nvidia-driver-510 openjdk-18-jdk p7zip pciutils pip powerline qt5-style-plugins rustc shotwell ubuntu-restricted-extras vulkan-tools
+apt install -y acpid awscli binutils cargo celluloid clojure curl dkms elixir ffmpeg fish fonts-firacode fonts-ibm-plex gcc golang htop linux-firmware make mesa-vulkan-drivers make npm nvidia-driver-510 openjdk-18-jdk p7zip pciutils pip powerline qt5-style-plugins rustc shotwell ubuntu-restricted-extras vulkan-tools
 
 
 apt remove -y gnome-photos gnome-system-monitor gnome-clocks gnome-system-monitor gnome-weather gnome-contacts yelp yelp-xsl eog cheese seahorse ubuntu-docs ubuntu-advantage-tools
