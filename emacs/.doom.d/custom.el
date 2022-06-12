9cd73838f8ad5de8ddfc3f7cf42cfa2504c2ff94
Gabriel Gian <gabrielgian@protonmail.com>
2022-06-12 11:32:45 -0300

    Hill

---
+++ emacs/.doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4699e3a86b1863bbc695236036158d175a81f0f3ea504e2b7c71f8f7025e19e3" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
