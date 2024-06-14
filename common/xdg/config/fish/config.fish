8aecdb89ebccb40133b734b148eb06e0a53dcba6
Gabriel Lemos <gabrielgian@protonmail.com>
2024-06-14 10:32:24 -0300

    Giorgio by Moroder

--- common/xdg/config/fish/config.fish
+++ common/xdg/config/fish/config.fish
@@ -7,11 +7,12 @@ export LC_CTYPE=pt_BR.UTF-8
 set -l eza_available (which eza)
 
 if test -n "$eza_available"
-  alias ls="eza --bytes --git --group-directories-first"
+    alias ls="eza --bytes --git --group-directories-first"
 end
 
 alias la="ls -lha"
 alias df="df -h"
 alias du="du -ch"
+alias tcpd="sudo tcpdump -A -s 10240 'tcp port 4080 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' | grep -E --line-buffered '^........(GET |HTTP/|POST |HEAD )|^[A-Za-z0-9-]+: ' | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g'"
 
 starship init fish | source
