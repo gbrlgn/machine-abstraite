96436c4d121c1f8e5f00f3614ed7683f047a270a
Gabriel Lemos <gabrielgian@protonmail.com>
2024-07-01 11:04:41 -0300

    Station to Station

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,11 +7,11 @@ mount -L boot /mnt/boot/efi
 
 if [ $1 = "keter" ]
 then
-	nixos-install --flake .#keter --root /mnt --no-root-password
+	nixos-install --flake ..#keter --root /mnt --no-root-password
 fi
 
 if [ $1 = "chokmah" ]
 then
-	nixos-install --flake .#chokmah --root /mnt --no-root-password
+	nixos-install --flake ..#chokmah --root /mnt --no-root-password
 fi
 
