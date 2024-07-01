d1cc5a8658284fe3994800c2fc334d912b01f558
Gabriel Lemos <gabrielgian@protonmail.com>
2024-07-01 12:04:14 -0300

    Swapped

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 
-mount -L root /mnt
-mkdir -p /mnt/boot/efi
+mount -L nixos /mnt
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/nix
+btrfs subvolume create /mnt/swap
+mkdir -p /mnt/boot/efi /mnt/{home,nix,swap}
+
 mount -L boot /mnt/boot/efi
+mount -o subvol=home,compress=zstd,noatime -L nixos /mnt/home
+mount -o subvol=nix,compress=zstd,noatime -L nixos /mnt/nix
+mount -o subvol=swap,compress=zstd,noatime -L nixos /mnt/swap
+btrfs filesystem mkswapfile --size 2g --uuid clear /mnt/swap/swapfile
 
 if [ $1 = "keter" ]
 then
