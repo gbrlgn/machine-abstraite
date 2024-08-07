8dad49d359fab8e9ddf2157080f0f6371804686f
Gabriel Lemos <gabrielgian@protonmail.com>
2024-08-07 09:16:30 -0300

    תֹהוּ וָבֹהוּ

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,19 +3,21 @@
 
 if [ $# -ne 1 ]
 then
-	echo "[sudo] ./rebuild.sh [local|remote]"
+	echo "[sudo] ./update.sh [local|remote]"
 	exit 1
 fi
 
 if [ "$1" = "local" ]
 then
 	nixos-rebuild switch --flake .#$(hostname)
+	flatpak upgrade -y
 	exit 0
 fi
 
 if [ "$1" = "remote" ]
 then
 	nixos-rebuild switch --flake github:gbrlgn/machine-abstraite/main#$(hostname)
+	flatpak upgrade -y
 	exit 0
 fi
 
