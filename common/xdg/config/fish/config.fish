7bfd421cd63d4d59f902ef2913687400ea7254d1
Gabriel Lemos <gabrielgian@protonmail.com>
2025-03-12 13:49:17 -0300

    תֹהוּ וָבֹהוּ

--- common/xdg/config/fish/config.fish
+++ common/xdg/config/fish/config.fish
@@ -11,4 +11,5 @@ alias du="du -ch"
 alias tcpd="sudo tcpdump -A -s 10240 'tcp port 4080 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' | grep -E --line-buffered '^........(GET |HTTP/|POST |HEAD )|^[A-Za-z0-9-]+: ' | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g'"
 alias assume="source $HOME/.config/fish/assume.fish"
 
+fnm env --use-on-cd --shell fish | source
 starship init fish | source
