496578f6ea895c1d6f0a2f220bb17bd1a5aa209c
Gabriel Lemos <gabrielgian@protonmail.com>
2025-11-17 15:38:04 -0300

    תֹהוּ וָבֹהוּ

--- common/xdg/config/fish/config.fish
+++ common/xdg/config/fish/config.fish
@@ -7,6 +7,8 @@ alias du="du -ch"
 alias tcpd="sudo tcpdump -A -s 10240 'tcp port 4080 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' | grep -E --line-buffered '^........(GET |HTTP/|POST |HEAD )|^[A-Za-z0-9-]+: ' | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g'"
 alias assume="source $HOME/.config/fish/assume.fish"
 
+bind alt-backspace backward-kill-word
+
 set fish_greeting
 
 set -l eza_available (which eza)
